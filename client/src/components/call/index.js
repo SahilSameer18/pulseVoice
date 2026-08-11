4eee8a4d8b6da8ca9fdc5858ea29c5c12e472f59
sahil sameer siddique <sahilsameer.dev18@gmail.com>
2026-08-11 13:07:22 +0530

    feat: export call components from a centralized index file

--- client/src/components/call/index.js
+++ client/src/components/call/index.js
@@ -1,1 +1,3 @@
-
+export { default as AudioVisualizer } from './AudioVisualizer';
+export { default as CallControls } from './CallControls';
+export { default as TranscriptView } from './TranscriptView';
